e2948125c28a7b87f36d7876067ca9490740b7ad
Victoria Nikolaeva <150319175+victoria-nikolaeva04@users.noreply.github.com>
2024-07-10 09:24:41 -0400

    Created footer and header componets

---
+++ components/footer.js
@@ -0,0 +1,155 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <style>
+            :root {
+                --off-white: #FFFAF1;
+                --blue: #586994;
+                --grey: #7D869C;
+                --brown: #342A21;
+                --green: #99B8A3;
+            }
+
+            body{
+                background-color: var(--off-white);
+                margin: 0px
+            }
+
+            a{
+                text-decoration: none;
+                color: var(--off-white);
+            }
+
+            .text{
+                font-family: 'Roboto Condensed';
+            }
+
+            .default-button{
+                background-color: var(--green);
+                color: var(--off-white);
+                border-radius: 16px;
+                border-width: 0;
+                padding: 7px 5px 7px 5px;
+                font-size: 15px;
+                cursor: pointer;
+            }
+
+            footer{
+                background: var(--grey);
+            }
+
+            .footer-container{
+                margin-left: 15px;
+                margin-right: 15px;
+                background: var(--grey);
+                display: flex;
+            }
+
+            .footer-column-logo{
+                float: left;
+            }
+
+            .footer-column-logo p{
+                margin-left: 12px;
+                color: var(--off-white);
+            }
+
+            .footer-column-links{
+                margin-bottom: 4px;
+                margin-left: 20px;
+                color: var(--off-white);
+            }
+
+            .footer-column-links::after{
+                color: var(--off-white);
+            }
+
+            .footer-column-links h4{
+                margin-bottom: 6px;
+                margin-left: 20px;
+            }
+
+            .footer-column-links li{
+                list-style-type: none;
+                margin-top: 2px;
+            }
+
+            .footer-column-links ul{
+                list-style-type: none;
+                text-decoration: none;
+                color: var(--off-white);
+            }
+
+            .footer-column-links ul:visited{
+                color: var(--off-white);
+            }
+
+            .footer-column-email{
+                font-family: 'Roboto Condensed';
+                color: var(--off-white);
+                position: absolute;
+                right: 16px;
+            }
+
+            .footer-column-email h4{
+                font-family: 'Roboto Condensed';
+                font-size: 15px;
+                margin-bottom: 0px;
+            }
+
+            .footer-column-email p{
+                font-family: 'Roboto Condensed';
+                margin-top: 2px;
+                font-size: 12px;
+            }
+
+            .footer-input{
+                border-width: 0px;
+                border-color: var(--off-white);
+            }
+
+
+            #logo-footer{
+                width: 100px;
+            }
+
+        </style>
+        <footer>
+            <div class="footer-container">
+                <div class="footer-column-logo">
+                    <img src="images/temp.png" alt="logo" class="logo" id="logo-footer">
+                    <p>copyright ©</p>
+                </div>
+
+                <div class="footer-column-links">
+                    <h4>Quick Links</h4>
+                    <ul style="margin-top: 3px;">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Product</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-column-email">
+                    <h4>Intrested in Store Updates?</h4>
+                    <p>Sign Up!</p>
+
+                    <form>
+                        <input type="email" class="footer-input" required>
+                    </form>
+                    <br>
+
+                    <button class="default-button" style="float: right;">Sign Up</button>
+                </div>
+
+            </div>
+        </footer>
+      `;
+    }
+  }
+  
+  customElements.define('footer-component', Footer);
---
+++ components/header.js
@@ -0,0 +1,184 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <style>
+            :root {
+                --off-white: #FFFAF1;
+                --blue: #586994;
+                --grey: #7D869C;
+                --brown: #342A21;
+                --green: #99B8A3;
+            }
+
+            body{
+                background-color: var(--off-white);
+                margin: 0px
+            }
+
+            a{
+                text-decoration: none;
+                color: var(--off-white);
+            }
+
+            .text{
+                font-family: 'Roboto Condensed';
+            }
+
+            .default-button{
+                background-color: var(--green);
+                color: var(--off-white);
+                border-radius: 16px;
+                border-width: 0;
+                padding: 7px 5px 7px 5px;
+                font-size: 15px;
+                cursor: pointer;
+            }
+
+            .logo{
+                border-radius: 50%;
+                width: 200px;
+                z-index: 2;
+                position: relative;
+                top: 10px;
+
+            }
+
+            #header-name{
+                font-family:'Roboto Condensed';
+                position: relative;
+                margin-left: 10px;
+                top: 24px;
+                z-index: 2;
+            }
+
+            #header-name h1 {
+                margin-bottom: 0px;
+                font-weight:800;
+            }
+
+            #header-name h4 {
+                font-style: italic;
+                margin-top: 3px;
+            }
+
+            .nav-container{
+                position: relative;
+                margin-left: 3%;
+                margin-top: 70px;
+                display: block;
+            }
+            .toggle-button {
+                display: none;
+                background: none;
+                border: none;
+                font-size: 1.5em;
+                cursor: pointer;
+                padding: 5px;
+            }
+
+            .nav-button{
+                background-color: var(--blue);
+                color: var(--off-white);
+                border-radius: 16px;
+                margin-right: 15px;
+                width: 120px;
+                align-items: baseline;
+                padding: 8px 15px 8px 15px;
+                border-width: 0px;
+                font-size: 15px;
+            }
+
+            /* Toggle Nav */
+            @media screen and (max-width: 750px) {
+                .nav-container {
+                    display: none;
+                }
+
+                .nav-bar{
+                    display: flex;
+                    flex-direction: column;
+                    margin-left: 20%;
+                    margin-bottom: 5%;
+                    position: absolute;
+                    left: 25px;
+                    bottom: 50px;
+                    
+                }
+
+                .nav-button{
+                    margin-bottom: 5px;
+                    width: 5%s;
+                }
+
+                .toggle-button {
+                    display: block;
+                }
+
+                .icon{
+                    border-radius: 100%;
+                    background-color: var(--blue);
+                    color: var(--off-white);
+                    width:3%;
+                    height: 5%;
+                    padding: 5px;
+                }
+
+                #sign-up-icon{
+                    position: absolute;
+                    top: 8px;
+                    right: 16px;
+                }
+
+                .header-container{
+                    margin-top: 15px;
+                    margin-left: 15px;
+                    display: flex;
+                    flex-direction: row;
+                }
+
+                .header-border{
+                    background-color: var(--grey);
+                    text-align: center;
+                    font-size: 10px;
+                    padding: 8px;
+                    position: relative;
+                    top: -33px;
+                    z-index: 1;
+                }
+
+                #toggle-icon{
+                    display: none;
+                }
+        </style>
+        <header>
+            <div class="header-container">
+                <img src="images/temp.png" alt="logo" class="logo">
+            
+                <div id="header-name">
+                    <h1>PIN UP</h1>
+                    <h4>Let's Put a Pin in It!</h4>
+                    <button class="toggle-button" onclick="toggleNav()">☰</button>
+                </div>
+
+                <div class="nav-container" id="navContainer">
+                    <nav class="nav-bar" id="navBar">
+                        <a href="#" class="nav-link"><button class="nav-button">Home</button></a>
+                        <a href="#" class="nav-link"><button class="nav-button">Product</button></a>
+                        <a href="#" class="nav-link"><button class="nav-button">Contact</button></a>
+                    </nav>
+                </div>
+
+                <!-- Work on Icons -->
+                <img src="images/icons/user.png" alt="signIn" class="icon" id="sign-up-icon"> 
+            </div>
+        </header>
+        <div class="header-border"></div>
+      `;
+    }
+  }
+  
+  customElements.define('header-component', Header);
