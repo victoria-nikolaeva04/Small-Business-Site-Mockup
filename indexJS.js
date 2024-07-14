c8010290e81e418008d1d2a3b8907e09ff394e21
Victoria Nikolaeva <150319175+victoria-nikolaeva04@users.noreply.github.com>
2024-07-14 14:56:01 -0400

    Finished Best Seller Slideshow

--- indexJS.js
+++ indexJS.js
@@ -14,21 +14,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const nextButton = document.getElementById('next-button');
     const slideshowContent = document.querySelector('.slideshow-content');
     let currentIndex = 0;
-    const itemsToShow = window.innerWidth <= 480 ? 1 : window.innerWidth <= 768 ? 2 : 4;
+    let itemsToShow = window.innerWidth <= 480 ? 1 : window.innerWidth <= 768 ? 2 : 4;
     const itemWidth = slideshowContent.querySelector('.grid-item').offsetWidth;
 
     prevButton.addEventListener('click', () => {
-        if (currentIndex > 0) {
-            currentIndex--;
-            updateSlidePosition();
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = slideshowContent.children.length - itemsToShow;
         }
+        updateSlidePosition();
     });
 
     nextButton.addEventListener('click', () => {
-        if (currentIndex < slideshowContent.children.length - itemsToShow) {
-            currentIndex++;
-            updateSlidePosition();
+        currentIndex++;
+        if (currentIndex > slideshowContent.children.length - itemsToShow) {
+            currentIndex = 0;
         }
+        updateSlidePosition();
     });
 
     function updateSlidePosition() {
@@ -36,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     window.addEventListener('resize', () => {
-        const itemsToShow = window.innerWidth <= 480 ? 1 : window.innerWidth <= 768 ? 2 : 4;
+        itemsToShow = window.innerWidth <= 480 ? 1 : window.innerWidth <= 768 ? 2 : 4;
         updateSlidePosition();
     });
 });
